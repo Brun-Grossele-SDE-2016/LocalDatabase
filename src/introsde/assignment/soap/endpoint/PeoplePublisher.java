560bc761febed30007b742e08605a85cc021729a
ABrun93 <a.brun93@gmail.com>
2017-03-07 10:55:33 +0100

    Third assignment server

---
+++ src/introsde/assignment/soap/endpoint/PeoplePublisher.java
@@ -0,0 +1,36 @@
+package introsde.assignment.soap.endpoint;
+
+import introsde.assignment.soap.webservice.PeopleImplementation;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URISyntaxException;
+
+import javax.xml.ws.Endpoint;
+
+public class PeoplePublisher 
+{
+    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
+    {
+        String PROTOCOL = "http://";
+        String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
+        String PORT = "6903";
+        String BASE_URL = "/ws/people";
+        
+        if (HOSTNAME.equals("127.0.0.1"))
+        {
+            HOSTNAME = "localhost";
+        }       
+
+        if (String.valueOf(System.getenv("PORT")) != "null")
+        {
+            PORT = String.valueOf(System.getenv("PORT"));
+        }
+        
+        String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
+        
+        System.out.println("Starting People Service...");
+        System.out.println("--> Published. Check out " + endpointUrl + "?wsdl");
+        Endpoint.publish(endpointUrl, new PeopleImplementation());
+    }
+}
